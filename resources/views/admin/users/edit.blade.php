e162b58bc2b2dcf10fbba03fc97bcb1e050edfd5
LeoLopeso <leonardo.lopes@corpstek.com.br>
2022-01-22 13:10:36 -0300

    Update dos usuários cadastrados

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -7,10 +7,10 @@
 @endsection
 
 @section('content')
-    
+
     @if($errors->any())
         <div class="alert alert-danger">
-            <ul>                
+            <ul>
                 <h5><i class="icon fas fa-ban"></i> Ocorreu um erro.</h5>
             @foreach($errors->all() as $error)
                 <li>{{$error}}</li>
@@ -22,21 +22,22 @@
     <div class="card">
         <div class="card-body">
             <form action="{{route('users.update', ['user'=>$user->id])}}" method="POST" class="form-horizontal">
+                @method('PUT')
                 @csrf
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">Nome Completo</label>
                     <div class="col-sm-10">
-                        <input type="text" name="name" value="{{old('name')}}" class="form-control @error('name') is-invalid @enderror">
+                        <input type="text" name="name" value="{{$user->name}}" class="form-control @error('name') is-invalid @enderror">
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">E-mail</label>
                     <div class="col-sm-10">
-                        <input type="email" name="email" value="{{old('email')}}" class="form-control @error('email') is-invalid @enderror">
+                        <input type="email" name="email" value="{{$user->email}}" class="form-control @error('email') is-invalid @enderror">
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label class="col-sm-2 col-form-label">Senha</label>
+                    <label class="col-sm-2 col-form-label">Nova Senha</label>
                     <div class="col-sm-10">
                         <input type="password" name="password" class="form-control @error('password') is-invalid @enderror">
                     </div>
@@ -50,14 +51,14 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label"></label>
                     <div class="col-sm-10">
-                        <input type="submit" value="Cadastrar" class="btn btn-success">
+                        <input type="submit" value="Salvar" class="btn btn-success">
                     </div>
                 </div>
             </form><!-- form -->
         </div><!-- card-body -->
     </div><!-- card -->
 
-    
+
 
 
 @endsection
